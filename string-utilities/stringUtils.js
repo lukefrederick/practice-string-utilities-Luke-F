2763dcfb55c8bc3095f7cab157a8da7cd3799e00
lfrederick034 <lfrederick220@gmail.com>
2025-09-03 20:32:35 -0500

    Final submission

--- string-utilities/stringUtils.js
+++ string-utilities/stringUtils.js
@@ -1,3 +1,29 @@
 function capitalize(str) {
-    console.log("Test");
+    if (!str) return "";
+
+    return str[0].toUpperCase() + str.slice(1);
 }
+
+// console.log("Capitalize: " + capitalize("test"));
+
+function reverse(str) {
+    if (!str) return "";
+    return str.split('').reverse().join('');
+}
+
+// console.log("Reverse: " + reverse("test"));
+
+function contains(str, substr) {
+    if(!str || !substr) return false;
+
+    if(str.includes(substr)) {
+        return true;
+    }
+    else {
+        return false;
+    }
+}
+
+// console.log("Contains: " + contains("Test", "es"));
+
+module.exports = { capitalize, reverse, contains };
